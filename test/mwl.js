2c13e553d70f4ff56b2b0bfb97db6824343bf4a2
Dan Reeves <heydanreeves@gmail.com>
2016-09-06 21:00:15 +0100

    mwl middleware tests

---
+++ test/mwl.js
@@ -0,0 +1,78 @@
+import test from 'ava';
+import fetch from 'node-fetch';
+import startServer from './helpers';
+
+const projectDir = './fixtures/mwl';
+let s;
+
+test.before(async t => {
+    s = await startServer(projectDir);
+});
+
+test.after(t => {
+    s.server.close();
+});
+
+test(`/html loads`, t => {
+    return fetch(`http://localhost:${s.port}/html`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `html\n`);
+    });
+});
+
+test(`/html.html loads`, t => {
+    return fetch(`http://localhost:${s.port}/html.html`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `html\n`);
+    });
+});
+
+test(`/jade 404s`, t => {
+    return fetch(`http://localhost:${s.port}/jade`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
+
+test(`/jade.jade 404s`, t => {
+    return fetch(`http://localhost:${s.port}/jade.jade`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
+
+test(`/sass 404s`, t => {
+    return fetch(`http://localhost:${s.port}/sass`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
+
+test(`/sass.sass 404s`, t => {
+    return fetch(`http://localhost:${s.port}/sass.sass`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
+
+test(`/coffee 404s`, t => {
+    return fetch(`http://localhost:${s.port}/coffee`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
+
+test(`/coffee.coffee 404s`, t => {
+    return fetch(`http://localhost:${s.port}/coffee.coffee`)
+    .then((res) => res.text())
+    .then((body) => {
+        t.is(body, `Page Not Found`);
+    });
+});
