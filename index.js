5c9778613b09c0804fa5315d5c94e9aee7474477
Dan Reeves <heydanreeves@gmail.com>
2016-09-17 12:41:28 +0100

    lowercase filenames

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var regProjectFinder = require('./layers/regprojectfinder');
-var setup = require('./layers/setup');
-var underscore = require('./layers/underscore');
-var mwl = require('./layers/mwl');
-var static = require('./layers/static');
-var proc = require('./layers/process');
-var fallbacks = require('./layers/fallbacks');
+var setup            = require('./layers/setup');
+var underscore       = require('./layers/underscore');
+var mwl              = require('./layers/mwl');
+var static           = require('./layers/static');
+var proc             = require('./layers/process');
+var fallbacks        = require('./layers/fallbacks');
 
 exports.regProjectFinder = regProjectFinder;
 exports.setup            = setup;
