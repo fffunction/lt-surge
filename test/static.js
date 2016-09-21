80876f3e2159e9f3a2299dbdf9c075e50abbcd20
Dan Reeves <heydanreeves@gmail.com>
2016-09-21 18:05:49 +0100

    reformat/lint + add test for directory -> index redirect

--- test/static.js
+++ test/static.js
@@ -5,34 +5,46 @@ import startServer from './helpers';
 const projectDir = './fixtures/static';
 let s;
 
-test.before(async t => {
-    s = await startServer(projectDir);
+test.before(() => {
+    return startServer(projectDir)
+        .then((server) => {
+            s = server;
+            return server;
+        });
 });
 
-test.after(t => {
+test.after(() => {
     s.server.close();
 });
 
-test(`/textfile.txt loads`, t => {
-    return fetch(`http://localhost:${s.port}/textfile.txt`)
-    .then((res) => res.text())
-    .then((body) => {
-        t.is(body, `this is a text file\n`);
-    });
+test('/textfile.txt loads', (t) => {
+    fetch(`http://localhost:${s.port}/textfile.txt`)
+        .then(res => res.text())
+        .then((body) => {
+            t.is(body, `this is a text file\n`);
+        });
 });
 
-test(`/htmlfile.html loads`, t => {
+test('/htmlfile.html loads', (t) => {
     return fetch(`http://localhost:${s.port}/htmlfile.html`)
-    .then((res) => res.text())
-    .then((body) => {
-        t.is(body, `this is a html file\n`);
-    });
+        .then(res => res.text())
+        .then((body) => {
+            t.is(body, `this is a html file\n`);
+        });
 });
 
-test(`/htmlfile loads with implicit .html`, t => {
+test('/htmlfile loads with implicit .html', (t) => {
     return fetch(`http://localhost:${s.port}/htmlfile`)
-    .then((res) => res.text())
-    .then((body) => {
-        t.is(body, `this is a html file\n`);
-    });
+        .then(res => res.text())
+        .then((body) => {
+            t.is(body, `this is a html file\n`);
+        });
+});
+
+test('/dir loads redirects to /dir/ and loads /dir/index.html', (t) => {
+    return fetch(`http://localhost:${s.port}/dir`)
+        .then(res => res.text())
+        .then((body) => {
+            t.is(body, `index.html\n`);
+        });
 });
